ccd49abcec494034d23e0a37d112bc4d872b859d
danlikestocode <dan37aguilera+1@gmail.com>
2022-07-02 19:21:39 -0700

    Implementing discord slash command builder

--- dist/Init.js
+++ dist/Init.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Init = void 0;
-const localdeploycommands_1 = require("./localdeploycommands");
+const handler_1 = require("./handler");
 const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv').config();
 const log4js = require('log4js');
 const fs = require('node:fs');
 const path = require('node:path');
 const logger = log4js.getLogger();
-const registrar = new localdeploycommands_1.LocalDeployCommands();
+const registrar = new handler_1.LocalDeployCommands();
 //Main Class
 class Init {
     token;
@@ -31,18 +31,7 @@ class Init {
         client.once('ready', () => {
             logger.info("Sucessfully logged into discord!");
         });
-        client.on("messageCreate", (message) => {
-            // Here's I'm using one of An Idiot's Guide's basic command handlers. Using the PREFIX environment variable above, I can do the same as the bot token below
-            if (message.author.bot)
-                return;
-            if (message.content.indexOf(prefix.length) !== 0)
-                return;
-            const args = message.content.slice(prefix.length).trim().split(/ +/g);
-            const command = args.shift().toLowerCase();
-            if (command === "ping") {
-                message.reply("Pong!");
-            }
-        });
+        //need to put logic here
         logger.debug("Authenticating your clients token...");
         client.login(this.token);
     }
